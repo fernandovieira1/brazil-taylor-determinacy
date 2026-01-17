2d7379381894b70fa3f6ca4256f5915751aeba93
Fernando Vieira <fernandovieira1@outlook.com>
2026-01-17 03:16:35 -0300

    v_omega (leitura final 2 + revisão latex)

--- scripts/+modelo_nk3/driver.m
+++ scripts/+modelo_nk3/driver.m
@@ -11,9 +11,9 @@ tic0 = tic;
 global M_ options_ oo_ estim_params_ bayestopt_ dataset_ dataset_info estimation_info
 options_ = [];
 M_.fname = 'modelo_nk3';
-M_.dynare_version = '6.5';
-oo_.dynare_version = '6.5';
-options_.dynare_version = '6.5';
+M_.dynare_version = '6.0';
+oo_.dynare_version = '6.0';
+options_.dynare_version = '6.0';
 %
 % Some global variables initialization
 %
